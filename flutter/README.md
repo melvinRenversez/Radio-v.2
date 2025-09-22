b3ed9c00e7adbf6cbab973653d20f780c431513e
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2025-09-22 18:13:50 +0200

    init

---
+++ flutter/README.md
@@ -0,0 +1,3 @@
+# flutter_radio_v2
+
+A new Flutter project.
---
+++ flutter/android/app/src/main/kotlin/com/example/flutter_radio_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_radio_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
